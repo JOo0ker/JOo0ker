878b22d710860dcac9e5a74682bc8466eb200bce
unknown <624459425@qq.com>
2021-11-29 00:16:17 +0800

    2021年11月29日00:16:01

---
+++ Chapter15/test/test.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main()
+{
+	struct dat { short a; short b; };
+	long value = 0xA224B118;
+	dat* pd = reinterpret_cast<dat*>(&value);
+	std::cout << pd->a;
+
+	return 0;
+}
