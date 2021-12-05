6605037e3fa3c5f6326e8529274173f9c222e5b4
unknown <624459425@qq.com>
2021-12-05 16:02:34 +0800

    2021年12月5日16:02:18

---
+++ mainp/mainp.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <bitset>
+
+int main()
+{
+	using namespace std;
+	cout << "Enter an interger: ";
+	int n;
+	cin >> n;
+
+	cout << "n     n*n\n";
+	cout << n << "     " << n * n << " (decimal)\n";
+
+	// set to hex mode
+	cout << hex;
+	cout << n << "     ";
+	cout << n * n << " (hexadecimal)\n";
+
+	// set to octcal mode
+	cout << oct << n << "     " << n * n << " (octal)\n";
+
+	// set to bin mode
+	//cout << bitset << n << "     " << n * n << " (bin)\n";
+	
+	// alternative way to call a manipulator
+	dec(cout);
+	cout << n << "     " << n * n << " (decimal)\n";
+
+	return 0;
+}
