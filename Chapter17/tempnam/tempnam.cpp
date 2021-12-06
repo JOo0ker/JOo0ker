6fd5a90713f1aefcd92ece3820cd11dfbbd6fe1c
unknown <624459425@qq.com>
2021-12-06 23:37:22 +0800

    2021年12月6日23:37:10

---
+++ Chapter17/tempnam/tempnam.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+	using namespace std;
+	cout << "This system can generate up to " << TMP_MAX << " temporary names of up to " << L_tmpnam << " characters.\n";
+	char pszName[L_tmpnam] = { '\0' };
+	cout << "Here are ten names:\n";
+	for (int i = 0; i < 10; i++)
+	{
+		tmpnam_s(pszName);
+		cout << pszName << endl;
+	}
+
+	return 0;
+}
